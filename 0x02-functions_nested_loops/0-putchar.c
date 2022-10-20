d6ee4d34ea2c2238a4275125a32a10892e8d88a4
RevampedTes <iamteslim@gmail.com>
2022-10-19 17:36:20 -0700

    nestedD

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * main - prints out _putchar
- *Return: always (0)
+ * Return: always (0)
  */
 int main(void)
 {
@@ -12,6 +13,8 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r');					
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
