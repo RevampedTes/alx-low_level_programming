6e1c44636c6d1aa74beae8471d6a697cd15574c0
RevampedTes <iamteslim@gmail.com>
2022-10-20 06:07:42 -0700

    mastering c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line using putchar
- * Return: Always 0 (Success)
+ * main - who im i if not seen through your bright eyes
+ * lyric from a song im vibing to
+ * Return: 0 if successful
  */
 int main(void)
 {
-	int n;
-
-	for (n = 48, n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar('\n');
-	return (0);
+int a = '0';
+for (a = '0', a <= '9'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
 }
