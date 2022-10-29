91a2156f05bb7b753ec4e2af1c055a7978b68c4d
RevampedTes <iamteslim@gmail.com>
2022-10-29 12:16:56 -0700

    task 0 to task 11 of 0x05.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,26 +2,43 @@
 
 /**
  * _atoi - converts a string to an integer
- * @s: the string
- * Return: value
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
-	int sign = l;
-	unsigned int num = 0;
+	int i, d, n, len, f, digit;
 
-	while (!('0' <= *s && *s <= '9') && *s != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+		++d;
+
+	if (s[i] >= '0' && s[i] <= '9')
 	{
-		if (*s == '-')
-			sign *= -1;
-		if (*s == '+')
-			sign *= +1;
-		s++;
+		digit = s[i] - '0';
+		if (d % 2)
+			digit = -digit;
+		n = n * 10 + digit;
+		f = 1;
+		if (s[i + 1] < '0' || s[i + 1] > '9')
+			break;
+		f = 0;
 	}
-	while ('0' <= *s && *s <= '9' && *s != '\0')
-	{
-		num = (num * 10) + (*s - '0');
-		s++;
-	}
-	return (num * sign);
+	i++;
+}
+
+if (f == 0)
+	return (0);
+
+return (n);
 }
