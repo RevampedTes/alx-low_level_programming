c7d7051cee927781af3302c8bcebfd64aaed385a
RevampedTes <iamteslim@gmail.com>
2022-10-31 21:06:05 -0700

    task 0 to task 9 of 0x07.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * *_memcpy - copies memory area
- * @dest: destination memory area
- * @src: memory area to copy from
- * @n: number of bytes to copy
- *
- * Return: pointer to dest
+ **_memcpy - copies memory area
+ *@dest: the memory area to be copied to
+ *@src: the memory area to be copied
+ *@n: the amount of bytes to be filled
+ *Return: (dest) a pointer to the memory area dest
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int  i;
 
 	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+		dest[i] = src[i];
 
-	return (s);
+	return (dest);
 }
