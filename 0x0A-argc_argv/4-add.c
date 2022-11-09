a9626d7b64c31f60c8a76483b6046decf6b81314
RevampedTes <iamteslim@gmail.com>
2022-11-09 14:31:05 -0800

    added task 0 to task 5

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds all arguments
+ * @argc: the number of arguments passed in
+ * @argv: an array of the arguments passed in
+ * Return: 0 on success, 1 if one of the arguments contains a symbol
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; *(argv[i] + j) != '\0'; j++)
+		{
+			if (!atoi(argv[i] + j))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+			sum += atoi(argv[i]);
+	}
+	printf("%i\n", sum);
+	return (0);
+}
