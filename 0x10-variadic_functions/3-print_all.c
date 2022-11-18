86ee0a38b811456dbdd7b7b269a2527480023c13
RevampedTes <60141269+RevampedTes@users.noreply.github.com>
2022-11-18 09:32:45 +0100

    Update 3-print_all.c
    added task 0 to task 3 of 0x10

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,90 @@
 #include "variadic_functions.h"
+
+/**
+ * print_char - prints a character
+ * @valist: the character to be printed
+ * Return: nothing
+ */
+void print_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+
+/**
+ * print_int - prints an integer
+ * @valist: the integer to be printed
+ * Return: nothing
+ */
+void print_int(va_list valist)
+{
+	printf("%i", va_arg(valist, int));
+}
+
+/**
+ * print_float - prints a floating point
+ * @valist: the float to be printed
+ * Return: nothing
+ */
+void print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+
+/**
+ * print_str - prints a string
+ * @valist: the string to be printed
+ * Return: nothing
+ */
+void print_str(va_list valist)
+{
+	char *cpy = va_arg(valist, char *);
+
+	if (cpy)
+	{
+		printf("%s", cpy);
+		return;
+	}
+	printf("(nil)");
+}
+
+/**
+ * print_all - prints anything, by calling other functions
+ * @format: a string containing the cariable types passed in to be printed
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j;
+	char *separator = "";
+	print_table p[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(valist, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (p[j].type)
+		{
+			if (*p[j].type == format[i])
+			{
+				printf("%s", separator);
+				p[j].func(valist);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(valist);
+
+	printf("\n");
+}
