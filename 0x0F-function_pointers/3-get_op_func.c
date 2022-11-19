478cb54526c1509ae82c5191f63d40faf493d267
RevampedTes <iamteslim@gmail.com>
2022-11-19 11:58:35 -0800

    added task 0 to task 4 of 0x0F.C

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,9 +1,12 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - selects a 3-calc.h function based on input parameter
- * @s: the character to compare against
- * Return: the function associated to @s, OR 1 if no matches
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
 
 int (*get_op_func(char *s))(int, int)
@@ -19,9 +22,10 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
+
 	while (ops[i].op)
 	{
-		if (!strcmp(s, ops[i].op))
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
