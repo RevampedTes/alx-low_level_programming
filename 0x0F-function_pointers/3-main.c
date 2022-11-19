2f82d7e43ec8d68844243d1c58a0511c941a4c01
RevampedTes <iamteslim@gmail.com>
2022-11-19 12:16:17 -0800

    added task 0 to task 4 of 0x0F.C

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,16 +1,18 @@
 #include "3-calc.h"
 
 /**
- * main - performs basic mathmatical operatons
- * @argc: the number of passed arguments
- * @argv: an array of the passed arguments
- * Return: Always 0
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	int a1, a2, output;
-	int (*math_func)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,16 +23,25 @@ int main(int argc, char *argv[])
 	a1 = atoi(argv[1]);
 	a2 = atoi(argv[3]);
 
-	math_func = get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 
-	if (!math_func)
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	output = math_func(a1, a2);
-	printf("%i\n", output);
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%d\n", result);
 
 	return (0);
 }
