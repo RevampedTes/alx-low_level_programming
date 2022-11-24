6523198f120c4d25080644f14b8b80a1f0b010ae
RevampedTes <60141269+RevampedTes@users.noreply.github.com>
2022-11-25 00:47:37 +0100

    Update 1-list_len.c
    0 to 6 of 0x12.C

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+
+/**
+ * list_len - returns the elements in a linked list_t list
+ * @h: the input list_t list
+ *
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
